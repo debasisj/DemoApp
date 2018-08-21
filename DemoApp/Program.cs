89a93eda92308880ab8fc95351650be26eed9f72
deb@Sympli <deb.jagadev@live.com>
2018-08-21 17:11:31 +1000

    first commit

---
+++ DemoApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Read();
+            Console.WriteLine(args[0]);
+            Console.WriteLine("=====================");
+            Console.WriteLine(args[1]);
+            Console.WriteLine("=====================");
+            Console.WriteLine("\nGive the URL");
+            var url = Console.ReadLine();
+            
+            
+               InitDriver.Init(args[0], args[1]);
+            
+           
+        }
+    }
+}
---
+++ Driver/InitDriver.cs
@@ -0,0 +1,41 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApp
+{
+    public class InitDriver
+    {
+        private static IWebDriver webDriver;
+        public static void Init(string driverTyper, string url )
+        {
+           
+            switch (driverTyper)
+            {
+                case "chrome":
+                    webDriver = new ChromeDriver(@"C:\Users\deb.jagadeba\source\repos\DemoApp\Driver");
+                    break;
+
+                case "firefox":
+                    webDriver = new FirefoxDriver(@"C:\Users\deb.jagadeba\source\repos\DemoApp\Driver");
+                    break;
+
+                case "ie":
+                    webDriver = new InternetExplorerDriver(@"C:\Users\deb.jagadeba\source\repos\DemoApp\Driver");
+                    break;
+            }
+          
+            webDriver.Navigate().GoToUrl(url);
+            webDriver.Manage().Window.Maximize();
+            webDriver.Close();
+            webDriver.Quit();
+
+        }
+    }
+}
